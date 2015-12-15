bfa2ee2f21256eedae5c35951302cd52484fd09b
Denis Mihaylov <denis.mihaylov93@gmail.com>
2015-12-15 19:19:23 +0200

    Creates structures of LazyMode

--- Ruby/exercises/seventh_homework.rb
+++ Ruby/exercises/seventh_homework.rb
@@ -1,17 +1,42 @@
 module LazyMode
 
-  class Date
-    attr_accessor :year, :month, :day
-    def initialize(date_string)
-      @year, @month, @day = date_string.split('-').map {|part| part.to_i}
-    end
+  class << self
 
-    def to_s
-      "#{'%04d' % @year}-#{'%02d' % @month}-#{'%02d' % @day}"
+    def create_file(file_name)
+      File.new(file_name, &(proc))
     end
 
   end
 
 end
 
+class LazyMode::Date
+  attr_accessor :year, :month, :day
+  def initialize(date_string)
+    @year, @month, @day = date_string.split('-').map {|part| part.to_i}
+  end
 
+  def to_s
+    "#{'%04d' % @year}-#{'%02d' % @month}-#{'%02d' % @day}"
+  end
+
+end
+
+class LazyMode::File
+
+  def initialize(file_name)
+    @name = file_name
+    instance_eval(&(proc))
+  end
+
+  private
+
+  def note
+    
+  end
+
+end
+
+class LazyMode::Note
+
+end
