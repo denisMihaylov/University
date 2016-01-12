42202eccc3ad56396bf9cb04b5fdf555958aa10d
Denis Mihaylov <denis.mihaylov93@gmail.com>
2016-01-12 03:58:44 +0200

    Almost finishes File Downloader

--- Networking/FileDownloader/client.cpp
+++ Networking/FileDownloader/client.cpp
@@ -5,40 +5,84 @@
 #include <string.h>
 #include <netdb.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 
 #include <arpa/inet.h>
 
-#define PORT 4444
+#define PORT 5557
 #define MAXDATASIZE 65536
-#define INET_ADDR "10.255.3.96"
+#define INET_ADDR "192.168.0.107"
+#define MAX_DOWNLOADS 10
 
-int split_string(char* message, char** result) {
-    char* pch;
-    int i = 0;
-    pch = strtok(message, "\n");
-    while (pch != NULL) {
-        result[i++] = pch;
-        pch = strtok(NULL, "\n");
-    }
-    return i;
-}
+int my_fd, numbytes;
+char buf[MAXDATASIZE + 1];
+char* directory[50];
+int directory_count;
+size_t file_size[MAX_DOWNLOADS];
+char download_file_name[MAX_DOWNLOADS][50];
+char* input;
+int pipes[MAX_DOWNLOADS][2];
+int download = 0;
+size_t size;
 
-void printHelp();
+int split_string(char* string, char** result, char split);
+
+void print_help();
+
+void send_to_server(int my_fd, char* input);
+
+int recv_from_server(int my_fd, void *buf);
+
+void print_directories();
+
+void reset_directories();
+
+void handle_input();
+
+bool is_directory_valid(char* directory);
+
+void start_download();
+
+int connect_to_port(int port);
 
 int main() {
     system("clear");
     printf("Welcome to the File Downloader!\n\tCreated by: Denis Mihaylov\n");
-    int my_fd, numbytes;
-    char buf[MAXDATASIZE + 1];
+    my_fd = connect_to_port(PORT);
+    
+    input = (char *)malloc(MAXDATASIZE * sizeof(char));
+    printf("\nHere is the complete list of available directories\n"
+              "Note: Files have extensions, directories do not!\n");
+    recv_from_server(my_fd, buf);
+    print_directories();
+    while(strcmp(input, "exit")) {
+        printf("\nCommand (for help type \"help\"): ");
+        memset(input, 0, sizeof(input));
+        getline(&input, &size, stdin);
+        input[strlen(input) - 1] = '\0';
+        if (!strcmp(input, "help")) {
+            print_help();
+        } else if (strcmp(input, "exit")) {
+            handle_input();
+        }
+    }
+
+    return 0;
+}
+
+int connect_to_port(int port) {
+    int my_fd;
     struct sockaddr_in my_sock;
     my_sock.sin_family = AF_INET;
     if (inet_pton(AF_INET, INET_ADDR, &(my_sock.sin_addr)) != 1) {
-        fprintf(stderr, "Invalid address\n");
+        perror("Invalid address");
         return 1;
     }
-    my_sock.sin_port = htons(PORT);
+    my_sock.sin_port = htons(port);
 
     if((my_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("socket");
@@ -50,35 +94,153 @@ int main() {
         perror("client: connect");
         return 1;
     }
-    
-    char input[MAXDATASIZE];
-    printf("\nHere is the complete list of available directories\n"
-              "Note: Files have extensions, directories do not!\n");
+    return my_fd;
+}
+
+void handle_input() {
+    if (!strncmp(input, "cd", 2)) {
+        if (strlen(input) <=4 || strcmp(&input[3], "..") && !is_directory_valid(&input[3])) {
+            printf("No such directory\n");
+            return;
+        }
+        send_to_server(my_fd, input);
+        reset_directories();
+        recv_from_server(my_fd, buf);
+        printf("Directories:\n");
+        print_directories();
+    } else if (!strncmp(input, "down", 4)) {
+        if (strlen(input) <= 6 || !is_directory_valid(&input[5])) {
+            printf("No such file in the directory\n");
+            return;
+        }
+        start_download();
+        printf("The downloading has started\n"
+               "To check the status of the downloading use the status command\n");
+    } else if (!strncmp(input, "status", 6)) {
+        if (download == 0) {
+            printf("No active downloads\n");
+        } else {
+            int i, j;
+            printf("%d\n", download);
+            for (i = 0, j = 0; i < download; i++) {
+                char progress[5];
+                lseek(pipes[i][0], 0, SEEK_SET);
+                int a = read(pipes[i][0], progress, 5);
+                progress[a] = '\0';
+                printf("%s: %s%%\n", download_file_name[i], progress);
+                if (strcmp(progress, "100")) {
+                    j++;
+                    if (j < i) {
+                        pipes[j][0] = pipes[i][0];
+                        pipes[j][1] = pipes[i][1];
+                        memset(download_file_name[j], 0, sizeof(download_file_name[j]));
+                        strcpy(download_file_name[j], download_file_name[i]);
+                    }
+                }
+            }
+            download = j;
+        }
+    } else {
+        printf("No such command\n");
+    }
+}
+
+void start_download() {
+    int current_download = download++;
+    memset(download_file_name[current_download], 0, sizeof(download_file_name[current_download]));
+    strcpy(download_file_name[current_download], &input[5]);
+    // starting download and getting file size
+    send_to_server(my_fd, input);
+    struct response {
+        size_t size;
+        char path[400];
+    };
+    response result;
+    recv_from_server(my_fd, &result);
+    file_size[current_download] = result.size;
+    pipe(pipes[current_download]);
+    if (!fork()) {
+        close(pipes[current_download][0]);
+        int download_fd = connect_to_port(PORT + 1);
+        char buffer[MAXDATASIZE + 1];
+        int bytes;
+        send_to_server(download_fd, result.path);
+        int file_fd = creat(download_file_name[current_download], 0744);
+        size_t total_bytes = 0;
+        int progress = 0;
+        while(progress < 100) {
+            bytes = recv_from_server(download_fd, buffer);
+            lseek(pipes[current_download][1], 0, SEEK_SET);
+            total_bytes += bytes;
+            progress = total_bytes*100/file_size[current_download];
+            write(file_fd, buffer, bytes);
+            char tmp[12]={0x0};
+            sprintf(tmp,"%d", progress);
+            write(pipes[current_download][1], tmp, strlen(tmp));
+            printf("%d, %s\n", (int)strlen(tmp), tmp);
+            char a[5];
+        }
+        close(file_fd);
+        exit(0);
+    }
+    close(pipes[current_download][1]);
+}
+
+void reset_directories() {
+    for (int i = 0; i < directory_count; i++) {
+        memset(directory[i], 0, sizeof(directory[i]));
+    }
+}
+
+bool is_directory_valid(char* checked_directory) {
+    for (int i = 0; i < directory_count; i++) {
+        if (!strncmp(directory[i], checked_directory, strlen(checked_directory))) {
+            if (strlen(checked_directory) < strlen(directory[i])) {
+                strcat(checked_directory, &directory[i][strlen(checked_directory)]);
+            }
+            return true;
+        }
+    }
+    return false;
+}
+
+void print_directories() {
+    directory_count = split_string(buf, directory, '\n');
+    for (int i = 0; i < directory_count; i++)
+        printf("%d. %s\n", i, directory[i]);
+}
+
+int split_string(char* string, char** result, char split) {
+    char* pch;
+    int i = 0;
+    pch = strtok(string, &split);
+    while (pch != NULL) {
+        result[i++] = pch;
+        pch = strtok(NULL, &split);
+    }
+    return i;
+}
+
+void send_to_server(int my_fd, char* input) {
+    if(send(my_fd, input, strlen(input), 0) == -1) {
+        perror("send");
+        exit(1);
+    }
+}
+
+int recv_from_server(int my_fd, void* buf) {
     if ((numbytes = recv(my_fd, buf, MAXDATASIZE, 0)) == -1) {
         perror("recv");
         exit(1);
     }
-    char* directory[15];
-    int n = split_string(buf, directory);
-    for (int i = 0; i < n; i++)
-        printf("%d. %s\n", i, directory[i]);
-    while(strcmp(input, "exit") != 0) {
-        printf("\nCommand (for help type \"help\"): ");
-        scanf("%s", input);
-        if (!strcmp(input, "help")) {
-            printHelp();
-        }
-        if (send(my_fd, input, strlen(input), 0) == -1)
-           perror("send");
-        
-        
-    }
-
-    return 0;
+    return numbytes;
 }
 
-void printHelp() {
+void print_help() {
     printf("Available Commands:\n\thelp - shows all available commands\n"
-           "\tcd _ - changes the directory to the one defined by _. Using \"cd ..\" will navigate to the parent directory\n"
-           "\t\n");
+           "\tcd DIR - changes the directory to the one defined by DIR. "
+           "Using \"cd ..\" will navigate to the parent directory(There is autocompletion!)\n"
+           "\tdown file_name path - downloads the file\n"
+           "\tstatus - checks the status of all the downloads\n"
+           "\texit - leaves the application");
 }
--- Networking/FileDownloader/server.cpp
+++ Networking/FileDownloader/server.cpp
@@ -13,23 +13,154 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define PORT 4444
+#define PORT 5557
 #define MAXDATASIZE 65536
 #define BACKLOG 10
 #define FILES_PATH "../"
 
 char* pwd[256];
 
-void send_file(const int sock_fd, const int file_fd) {
-    char buffer[MAXDATASIZE + 1];
-    while(read(file_fd, buffer, MAXDATASIZE) != 0) {
-        if (send(sock_fd, buffer, MAXDATASIZE, 0) == -1) {
-            perror("send");
-            close(file_fd);
-            exit(0);
+void send_directories(const int sock_fd, const char* path);
+
+void send_file(const int sock_fd, const int file_fd);
+
+int send_file_size(char* file_path, int sock_fd);
+
+void escape_spaces(char* input);
+
+int occurrances_in_string(char* input, char search);
+
+void open_new_connection();
+
+int main() {
+    char buf[MAXDATASIZE];
+    struct in_addr my_addr;
+    struct sockaddr_in my_sock;
+    memset(&my_sock, 0, sizeof(my_sock));
+    my_sock.sin_family = AF_INET;
+    my_sock.sin_addr.s_addr = INADDR_ANY;
+    if (fork()) {//listen for new connections
+        my_sock.sin_port = htons(PORT);
+
+        int my_fd, new_fd, numbytes;
+
+        if ((my_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+            perror("socket");
+            return 0;
+        }
+
+        if(bind(my_fd, (struct sockaddr *)&my_sock, sizeof my_sock) == -1) {
+            perror("bind");
+            return 0;
+        }
+
+        if (listen(my_fd, BACKLOG) == -1) {
+            perror("listen");
+            return 0;
+        }
+        struct sockaddr_storage their_addr;
+        socklen_t sin_size = sizeof(their_addr);
+
+        while(1){
+            new_fd = accept(my_fd, (struct sockaddr *)&their_addr, &sin_size);
+            if (new_fd == -1) {
+              perror("accept");
+              return 1;
+            }
+            if (!fork()) {
+                char path[] = "../";
+                send_directories(new_fd, path);
+                while ((numbytes = recv(new_fd, buf, MAXDATASIZE, 0)) != 0) {
+                    if (numbytes == -1) {
+                        perror("recv");
+                        return 1;
+                    }
+                    if(!strncmp(buf, "cd", 2)) {
+                        escape_spaces(&buf[3]);
+                        strcat(path, &buf[3]);
+                        strcat(path, "/");
+                        send_directories(new_fd, path);
+                    } else if (!strncmp(buf, "down", 4)) {
+                        char file_path[MAXDATASIZE];
+                        strcpy(file_path, path);
+                        strcat(file_path, &buf[5]);
+                        int file_fd = send_file_size(file_path, new_fd);
+                    }
+                    memset(buf, 0, sizeof(buf));
+                }
+                //handle closing of the connection from the client
+                close(my_fd);
+                close(new_fd);
+                return 0; 
+            }
+        }
+
+        return 0;
+    } else {//listen for new download requests
+        my_sock.sin_port = htons(PORT + 1);
+        int my_fd, new_fd, numbytes;
+        if ((my_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+            perror("socket");
+            return 0;
+        }
+
+        if(bind(my_fd, (struct sockaddr *)&my_sock, sizeof my_sock) == -1) {
+            perror("bind");
+            return 0;
+        }
+
+        if (listen(my_fd, BACKLOG) == -1) {
+            perror("listen");
+            return 0;
+        }
+        struct sockaddr_storage their_addr;
+        socklen_t sin_size = sizeof(their_addr);
+        while(1){
+            new_fd = accept(my_fd, (struct sockaddr *)&their_addr, &sin_size);
+            if (new_fd == -1) {
+              perror("accept");
+              return 1;
+            }
+            if (!fork()) {
+                char file_path[MAXDATASIZE];
+                if(recv(new_fd, file_path, sizeof(file_path), 0) == -1) {
+                    perror("recv");
+                    return 1;
+                }
+                printf("Request: %s\n", file_path);
+                int fd = open(file_path, O_RDONLY); 
+                send_file(new_fd, fd);
+                return 0; 
+            }
         }
     }
-    close(file_fd);
+}
+
+int send_file_size(char* file_path, int sock_fd) {
+    int file_fd = open(file_path, O_RDONLY);
+    struct stat st;
+    struct response {
+        size_t size;
+        char path[400];
+    };
+    response result;
+    fstat(file_fd, &st);
+    strcpy(result.path, file_path);
+    result.size = st.st_size;
+    if (send(sock_fd, &(result), sizeof(response), 0) == -1) {
+        perror("send");
+        close(file_fd);
+        exit(1);
+    }
+    return file_fd;
+}
+
+int occurrances_in_string(char* input, char search) {
+    int result = 0;
+    for (int i = 0; i < strlen(input); i++)
+        if (search == input[i])
+            result++;
+    return result;
 }
 
 void send_directories(const int sock_fd, const char* path) {
@@ -46,56 +177,29 @@ void send_directories(const int sock_fd, const char* path) {
     }    
 }
 
-int main() {
-    char buf[MAXDATASIZE];
-    struct in_addr my_addr;
-    struct sockaddr_in my_sock;
-    memset(&my_sock, 0, sizeof(my_sock));
-    my_sock.sin_family = AF_INET;
-    my_sock.sin_addr.s_addr = INADDR_ANY;
-    my_sock.sin_port = htons(PORT);
-
-    int my_fd, new_fd, numbytes;
-
-    if ((my_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("socket");
-        return 0;
-    }
-
-    if(bind(my_fd, (struct sockaddr *)&my_sock, sizeof my_sock) == -1) {
-        perror("bind");
-        return 0;
-    }
-
-    if (listen(my_fd, BACKLOG) == -1) {
-        perror("listen");
-        return 0;
-    }
-    struct sockaddr_storage their_addr;
-    socklen_t sin_size = sizeof(their_addr);
-
-    while(1){
-        new_fd = accept(my_fd, (struct sockaddr *)&their_addr, &sin_size);
-        if (new_fd == -1) {
-          perror("accept");
-          return 1;
-        }
-        if (!fork()) {
-            char path[] = "../";
-            send_directories(new_fd, path);
-            while ((numbytes = recv(new_fd, buf, MAXDATASIZE, 0)) != 0) {
-                if (numbytes == -1) {
-                    perror("recv");
-                    return 1;
-                }
-                //send_file(new_fd);
+void send_file(const int sock_fd, const int file_fd) {
+    if(fork()) {
+        char buffer[MAXDATASIZE + 1];
+        memset(buffer, 0, sizeof(buffer));
+        while(read(file_fd, buffer, MAXDATASIZE) != 0) {
+            if (send(sock_fd, buffer, strlen(buffer), 0) == -1) {
+                perror("send");
+                close(file_fd);
+                exit(0);
             }
-            //handle closing of the connection from the client
-            close(my_fd);
-            close(new_fd);
-            return 0; 
+            memset(buffer, 0, sizeof(buffer));
         }
+        close(file_fd);
+        exit(0);
     }
+}
 
-    return 0;
+void escape_spaces(char* input) {
+    char* result = (char*)malloc(strlen(input) + occurrances_in_string(input, ' ') + 1); 
+    for(int i = 0, j = 0; i < strlen(input); i++, j++) {
+        if(input[i] == ' ')
+            result[j++] = '\\';
+        result[j] = input[i];
+    }
+    input = result;
 }
