9f34334154a9bbde6691e05999313cc18ed24a5d
Denis Mihaylov <denis.mihaylov93@gmail.com>
2016-02-06 21:31:36 +0200

    Implementation of the SQLite3 logger (not quite beautiful)

--- Ruby/log4ruby/lib/log4ruby/handler/sqlite3_handler.rb
+++ Ruby/log4ruby/lib/log4ruby/handler/sqlite3_handler.rb
@@ -3,26 +3,44 @@ require 'sqlite3'
 module Log4Ruby
   class SQLite3Handler < DBHandler
 
-    STATEMENT_START = "INSERT INTO #{Config.db[@type][:table_name]} 
-      (LOGGER_ID, LEVEL, TIME, EXCEPTION, MESSAGE) "
+    STATEMENT = "INSERT INTO #{Config.db[@type][:table_name]} (%s) %s"
+    FIRST_SELECT = "%s AS %s"
 
     def initialize
       @type, @queue = :sqlite3, []
     end
 
     def persist_messages
-      db = SQLite3::Database.open(Config.db[:sqlite3][:db_path])
-      db.execute("CREATE TABLE IF NOT EXISTS #{Config.db[@type][:table_name]}
-        (LOGGER_ID, LEVEL, TIME, EXCEPTION, MESSAGE)"
+      db = SQLite3::Database.open(Config.db[@type][:db_path])
+      db.execute(create_table_statement)
       db.execute(statement)
-    rescue SQLite3::Exception => e
+      @queue = []
+    rescue => e
       puts e
     ensure
       db.close if db
     end
 
     def statement
-      
+      columns = Config.message_formatters[@type][:parts]
+      STATEMENT % [columns.map(&:to_s).join(", "), select_statements(columns)]
+    end
+
+    def select_statements(columns)
+      message = @queue.shift
+      first_select = columns.map do |column|
+        FIRST_SELECT % ["'" + message.send(column).to_s + "'" || "'NIL'", column]
+      end.join(", ")
+      first_statement = "SELECT %s" % first_select
+      other_statements = @queue.map do |message|
+        "SELECT " + columns.map {|column| "'" + message.send(column).to_s + "'"||"'NILL'"}.join(", ")
+      end
+      other_statements.insert(0, first_statement).join(" UNION ALL ")
+    end
+
+    def create_table_statement
+      "CREATE TABLE IF NOT EXISTS #{Config.db[@type][:table_name]} (" +
+      Config.message_formatters[@type][:parts].join(", ") + ")"
     end
 
   end
