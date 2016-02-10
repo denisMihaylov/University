8188ce475e5341d69c20f34fa33cf3089dde34a8
Denis Mihaylov <denis.mihaylov93@gmail.com>
2016-02-11 00:44:50 +0200

    Adds {#level}_enabled? methods to logger class

--- ruby/log4ruby/lib/log4ruby/logger.rb
+++ ruby/log4ruby/lib/log4ruby/logger.rb
@@ -7,19 +7,17 @@ module Log4Ruby
       @level = level
     end
 
-    LEVELS[1..-2].each do |level|
-      define_method(level) do |message, exception = nil|
-        if logging_allowed?(level)
-          log(level, message, exception)
-        end
-      end
-    end
-
-
-    private
-
     def logging_allowed?(level)
       LEVELS.index(level) <= LEVELS.index(@level)
+    end
+
+    LEVELS[1..-2].each do |level|
+      define_method(level) do |message, exception = nil|
+        log(level, message, exception) if logging_allowed?(level)
+      end
+      define_method("#{level.to_s}_enabled?") do
+        logging_allowed?(level)
+      end 
     end
 
   end
