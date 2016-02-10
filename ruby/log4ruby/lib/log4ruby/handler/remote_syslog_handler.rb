cfcfd651c1c5b7c2c1e5069922a606fbe1cb5980
Denis Mihaylov <denis.mihaylov93@gmail.com>
2016-02-10 20:03:01 +0200

    Fixes remote handler to log properly when a facility is defined

--- ruby/log4ruby/lib/log4ruby/handler/remote_syslog_handler.rb
+++ ruby/log4ruby/lib/log4ruby/handler/remote_syslog_handler.rb
@@ -10,7 +10,8 @@ module Log4Ruby
 
     def log_message(message)
       level = get_level(message.level)
-      facility = "-n %s" % Config.syslog[:remote_host]
+      parts = [Config.syslog[:remote_host], message.logger_id.to_s, level]
+      facility = "-n %s.%s -p %s" % parts
       message = parse_message(message)
       call_bash_logging(facility, message)
     end
