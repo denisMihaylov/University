fd773c87da962eebd75d255a54134bf6230a89ea
Denis Mihaylov <denis.mihaylov93@gmail.com>
2016-03-13 01:30:37 +0200

    First homework

---
+++ python/tasks/solution.py
@@ -0,0 +1,42 @@
+def get_columns(matrix):
+    return [list(x) for x in zip(*matrix)]
+
+def rotate_left(picture):
+    columns = get_columns(picture)
+    return columns[::-1]
+
+def rotate_right(picture):
+    columns = get_columns(picture)
+    return list(map(lambda column: column[::-1], columns))
+
+def map_picture(func, picture):
+    def map_pixel(pixel):
+        return tuple(func(index, color) for index, color in enumerate(pixel))
+    return [list(map(map_pixel, row)) for row in picture]
+
+def invert(picture):
+    invert_color = lambda _, color: 255 - color
+    return map_picture(invert_color, picture)
+
+def lighten(picture, factor):
+    lighten_color = lambda _, color: int(color + factor * (255 - color))
+    return map_picture(lighten_color, picture)
+
+def darken(picture, factor):
+    darken_color = lambda _, color: int(color - factor*color)
+    return map_picture(darken_color, picture)
+
+def create_histogram(picture):
+    histogram = {
+        0 : dict.fromkeys(range(0, 256), 0),
+        1 : dict.fromkeys(range(0, 256), 0),
+        2 : dict.fromkeys(range(0, 256), 0),
+    }
+    def add_color(index, color):
+        histogram[index][color] += 1
+    map_picture(add_color, picture)
+    return {
+        "red" : histogram[0],
+        "green" : histogram[1],
+        "blue" : histogram[2],
+    }
