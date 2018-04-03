be3bbddadb5b72eb702c7cf981339f3b9c058d1f
Denis Mihaylov <denis.mihaylov93@gmail.com>
2018-04-03 11:55:46 +0300

    Finished SVM Loss Naive

--- masters/dl-tf/assignment1/deep_learning_su/classifiers/linear_svm.py
+++ masters/dl-tf/assignment1/deep_learning_su/classifiers/linear_svm.py
@@ -2,6 +2,7 @@ import numpy as np
 from random import shuffle
 from past.builtins import xrange
 
+
 def svm_loss_naive(W, X, y, reg):
   """
   Structured SVM loss function, naive implementation (with loops).
@@ -34,14 +35,18 @@ def svm_loss_naive(W, X, y, reg):
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
+        dW[:, j] += X[i]
+        dW[:, y[i]] -= X[i]
         loss += margin
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
+  dW += reg * 2 * W
 
   #############################################################################
   # TODO:                                                                     #
